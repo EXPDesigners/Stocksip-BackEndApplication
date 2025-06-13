c6967b322f6c3ef31c463aeb85464468b8d92e0c
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-13 18:44:20 -0500

    feat(products): add brand name enumeration type.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/ValueObjects/EBrandName.cs
@@ -0,0 +1,20 @@
+﻿namespace StockSip.Platform.API.InventoryManagement.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents the various brand names of a liquor.
+/// </summary>
+public enum EBrandName
+{
+    Tabernero,
+    SantiagoQueirolo,
+    Porton,
+    Cristal,
+    JohnnieWalker,
+    JackDaniels,
+    Budweiser,
+    Heineken,
+    Corona,
+    PilsenCallao,
+    Cusqueña,
+    Cartavio
+}
