b64e8616d7f036d642f818258695be8cff274191
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-13 18:14:41 -0500

    feat(products): add product state enumeration type.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/ValueObjects/EProductState.cs
@@ -0,0 +1,13 @@
+﻿namespace StockSip.Platform.API.InventoryManagement.Domain.Model.ValueObjects;
+
+/// <summary>
+/// This enum defines the possible states of liquor.
+/// The possible states are:
+/// - WITH_STOCK: When the product has enough stocks to do the normal operations with it.
+/// - OUT_OF_STOCK: When the product does not have a stock or is equal to 0.
+/// </summary>
+public enum EProductState
+{
+    OutOfStock,
+    InStock
+}
