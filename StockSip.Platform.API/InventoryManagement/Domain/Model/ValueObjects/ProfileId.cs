2be12d95ddc5bedc69a66feba4c570c82ebbda2e
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-13 18:03:21 -0500

    feat(products): add profile id value object.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/ValueObjects/ProfileId.cs
@@ -0,0 +1,9 @@
+﻿namespace StockSip.Platform.API.InventoryManagement.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents a unique identifier for a profile.
+/// </summary>
+/// <param name="Id">
+/// The unique identifier for the profile.
+/// </param>
+public record ProfileId(int Id);
