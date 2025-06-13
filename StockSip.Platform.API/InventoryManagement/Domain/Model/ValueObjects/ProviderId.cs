f4d5351d20c1af3b1a92198087883ae9baaefcde
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-13 18:16:21 -0500

    feat(products): add provider id value object.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/ValueObjects/ProviderId.cs
@@ -0,0 +1,9 @@
+﻿namespace StockSip.Platform.API.InventoryManagement.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents a unique identifier for a provider who is a user.
+/// </summary>
+/// <param name="Id">
+/// The unique identifier for the provider.
+/// </param>
+public record ProviderId(int Id);
