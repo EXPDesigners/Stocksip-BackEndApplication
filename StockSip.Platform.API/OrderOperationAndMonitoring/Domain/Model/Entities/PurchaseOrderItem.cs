ff3c74993043b68af4ad554b4a91c572db2b341e
Camila Huamani <jyb990106@gmail.com>
2025-06-14 12:44:25 -0500

    feat(orders): add purchase order item entity.

--- StockSip.Platform.API/OrderOperationAndMonitoring/Domain/Model/Entities/PurchaseOrderItem.cs
+++ StockSip.Platform.API/OrderOperationAndMonitoring/Domain/Model/Entities/PurchaseOrderItem.cs
@@ -1,6 +1,18 @@
+using StockSip.Platform.API.Shared.Domain.Model.ValueObjects;
+
 namespace StockSip.Platform.API.OrderOperationAndMonitoring.Domain.Model.Entities;
 
-public class PurchaseOrderItem
+public class PurchaseOrderItem(
+    int id,
+    int purchaseOrderId,
+    string catalogItemId,
+    int quantity,
+    Money unitPrice
+)
 {
-    
+    public int Id { get; } = id;
+    public int PurchaseOrderId { get; } = purchaseOrderId;
+    public string CatalogItemId { get; } = catalogItemId;
+    public int Quantity { get; } = quantity;
+    public Money UnitPrice { get; } = unitPrice;
 }
