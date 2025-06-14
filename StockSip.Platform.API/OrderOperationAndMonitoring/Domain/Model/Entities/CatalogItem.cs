350a185f48d824bfdc5fe8e53948ac656abe88bf
Camila Huamani <jyb990106@gmail.com>
2025-06-14 12:43:15 -0500

    fix: update catalog item entity.

--- StockSip.Platform.API/OrderOperationAndMonitoring/Domain/Model/Entities/CatalogItem.cs
+++ StockSip.Platform.API/OrderOperationAndMonitoring/Domain/Model/Entities/CatalogItem.cs
@@ -3,14 +3,20 @@ using StockSip.Platform.API.Shared.Domain.Model.ValueObjects;
 namespace StockSip.Platform.API.OrderOperationAndMonitoring.Domain.Model.Entities;
 
 public class CatalogItem(
-    string Id, 
-    string name, 
+    string id,
+    string name,
     string brand,
     int content,
     int catalogId,
     string productType,
     Money unitPrice
-    )
+)
 {
-    
+    public string Id { get; } = new(id);
+    public string Name { get; } = name;
+    public string Brand { get; } = brand;
+    public int Content { get; } = content;
+    public int CatalogId { get; } = catalogId;
+    public string ProductType { get; } = productType;
+    public Money UnitPrice { get; } = unitPrice;
 }
