039907bcdc63ff86b8d0b86791b8875b6e71d2d9
Camila Huamani <jyb990106@gmail.com>
2025-06-14 11:51:30 -0500

    feat(orders): add catalog item id value object.

--- StockSip.Platform.API/InventoryManagement/Domain/Model/ValueObjects/CatalogId.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StockSip.Platform.API.InventoryManagement.Domain.Model.ValueObjects;
-
-/// <summary>
-/// Represents a unique identifier for a catalog.
-/// </summary>
-/// <param name="Id">
-/// The unique identifier for the catalog.
-/// </param>
-public record CatalogId(int Id);
---
+++ StockSip.Platform.API/orderOperationAndMonitoring/Domain/Model/ValueObjects/PurchaseOrderId.cs
@@ -0,0 +1,9 @@
+namespace StockSip.Platform.API.orderOperationAndMonitoring.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents a unique identifier for a purchase order.
+/// </summary>
+/// <param name="Id"></param>
+/// The unique identifier for the purchase order.
+/// </param
+public record PurchaseOrderId(int Id);
