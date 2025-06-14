b21436cf2d7db76cbb4781458c91e0ca41b29111
Camila Huamani <jyb990106@gmail.com>
2025-06-14 12:21:14 -0500

    feat(catalog): add catalog item type

--- StockSip.Platform.API/OrderOperationAndMonitoring/Domain/Model/Entities/CatalogItem.cs
+++ StockSip.Platform.API/OrderOperationAndMonitoring/Domain/Model/Entities/CatalogItem.cs
@@ -1,6 +1,16 @@
+using StockSip.Platform.API.Shared.Domain.Model.ValueObjects;
+
 namespace StockSip.Platform.API.OrderOperationAndMonitoring.Domain.Model.Entities;
 
-public class CatalogItem
+public class CatalogItem(
+    string Id, 
+    string name, 
+    string brand,
+    int content,
+    int catalogId,
+    string productType,
+    Money unitPrice
+    )
 {
     
 }
