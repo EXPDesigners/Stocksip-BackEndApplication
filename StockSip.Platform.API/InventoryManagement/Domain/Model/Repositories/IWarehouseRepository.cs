30297cdec296a6d5b1fea788900ab92ee078a4fd
Far14z <faridce14@gmail.com>
2025-06-14 12:55:11 -0500

    feat(warehouse): add warehouse outbound service contracts.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/Repositories/IWarehouseRepository.cs
@@ -0,0 +1,9 @@
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
+using StockSip.Platform.API.Shared.Domain.Repositories;
+
+namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Repositories;
+
+public interface IWarehouseRepository : IBaseRepository<Warehouse>
+{
+    
+}
