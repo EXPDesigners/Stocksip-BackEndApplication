424ed5247b64066c58563981ea7e90fd3fb558bb
Far14z <faridce14@gmail.com>
2025-06-16 18:47:51 -0500

    feat(warehouse): add command service contract.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/Services/IWarehouseCommandService.cs
@@ -0,0 +1,9 @@
+﻿using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Commands;
+
+namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Services;
+
+public interface IWarehouseCommandService
+{
+    Task<Warehouse?> handle(CreateWarehouseCommand command); 
+}
