b7f50c53c994039c5d071ae2521a24de61ade7de
Far14z <faridce14@gmail.com>
2025-06-16 17:06:29 -0500

    feat(warehouse): add constructor for the warehouse and create warehouse command.

--- StockSip.Platform.API/InventoryManagement/Domain/Model/Aggregates/Warehouse.cs
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/Aggregates/Warehouse.cs
@@ -7,23 +7,49 @@ namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
 /// <summary>
 /// This class defines the properties and behaviors of a warehouse, including its name, address, temperature range, capacity, and associated profile ID.
 /// </summary>
-public class Warehouse()
+public class Warehouse
 {
-    public long WarehouseId { get; }
-    public string Name { get; private set; }
-    public WarehouseAddress Address { get; internal set; }
-    public Temperature Temperature { get; internal set; }
-    public Capacity Capacity { get; internal set; }
+    public int WarehouseId { get; internal set; }
+    public string? Name { get; private set; }
+    public WarehouseAddress? Address { get; internal set; }
+    public Temperature? Temperature { get; internal set; }
+    public Capacity? Capacity { get; internal set; }
     
-    public string imageUrl { get; private set;  }
+    public ImageUrl? ImageUrl { get; internal set;  }
     
-    public ProfileId Id { get; internal set; }
+    public ProfileId ProfileId { get; internal set; }
 
-    public Warehouse(CreateWarehouseCommand command) : this()
+
+    /// <summary>
+    /// The constructor initializes a new instance of the Warehouse class with the specified parameters.
+    /// </summary>
+    /// <param name="name">The name of the warehouse</param>
+    /// <param name="address">The address of the warehouse</param>
+    /// <param name="temperature">The address of the warehouse</param>
+    /// <param name="capacity">The capacity of the warehouse</param>
+    /// <param name="imageUrl">The image of the warehouse</param>
+    /// <param name="profileId">The profile to which the warehouse belongs </param>
+    private Warehouse(string name, WarehouseAddress address, Temperature temperature, Capacity capacity,
+        ImageUrl imageUrl, ProfileId profileId)
     {
-        this.Name = command.Name;
-        this.Address = command.Address;
-        this.Temperature = command.Temperature;
-        this.Capacity = command.Capacity;
+        Name = name;
+        Address = address;
+        Temperature = temperature;
+        Capacity = capacity;
+        ImageUrl = imageUrl;
+        ProfileId = profileId;
     }
+    
+    /// <summary>
+    /// The constructor initializes a new instance of the Warehouse class using a CreateWarehouseCommand.
+    /// </summary>
+    /// <param name="command">The command to create a warehouse</param>
+    public Warehouse(CreateWarehouseCommand command) : this(
+        command.Name,
+        new WarehouseAddress(command.Street, command.City, command.District, command.PostalCode, command.Country),
+        new Temperature(command.MinTemperature, command.MaxTemperature),
+        new Capacity(command.Capacity),
+        new ImageUrl(command.ImageUrl),
+        new ProfileId(command.ProfileId)) 
+    {}
 }
--- StockSip.Platform.API/InventoryManagement/Domain/Model/Commands/CreateWarehouseCommand.cs
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/Commands/CreateWarehouseCommand.cs
@@ -2,13 +2,15 @@ using StockSip.Platform.API.InventoryManagement.Domain.Model.ValueObjects;
 
 namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Commands;
 
-/// This command is used to create a new warehouse in the system.
-/// <summary>
-/// this record defines the properties required to create a new warehouse, including its name, address, temperature range, capacity, and an image URL.
-/// </summary>
-/// <param name="Name">The name of the warehouse</param>
-/// <param name="Address">The address of the warehouse</param>
-/// <param name="Temperature">The temperature range in the warehouse</param>
-/// <param name="Capacity">The capacity of the warehouse</param>
-/// <param name="imageUrl">the image url of the warehouse</param>
-public record CreateWarehouseCommand(String Name, WarehouseAddress Address, Temperature Temperature, Capacity Capacity, string imageUrl);
+
+public record CreateWarehouseCommand(string Name, 
+                                     string Street,
+                                     string City,
+                                     string District,
+                                     string PostalCode,
+                                     string Country, 
+                                     double MinTemperature,
+                                     double MaxTemperature,
+                                     double Capacity,
+                                     string ImageUrl,
+                                     int ProfileId);
