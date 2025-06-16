f89ff435a74518a9e12f4982c0cd9961141fb248
Far14z <faridce14@gmail.com>
2025-06-16 18:46:13 -0500

    feat(warehouse): add outbound service contract.

--- StockSip.Platform.API/InventoryManagement/Domain/Model/Repositories/IWarehouseRepository.cs
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/Repositories/IWarehouseRepository.cs
@@ -1,9 +1,21 @@
 using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.ValueObjects;
 using StockSip.Platform.API.Shared.Domain.Repositories;
 
 namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Repositories;
 
+/// <summary>
+/// This interface defines the contract for a repository that manages Warehouse aggregates.
+/// </summary>
 public interface IWarehouseRepository : IBaseRepository<Warehouse>
 {
+    /// <summary>
+    /// This method checks if a warehouse with the specified name and profile ID exists in the database.
+    /// </summary>
+    Task<bool> ExistByNameAndProfileIdAsync(string name, ProfileId profileId);
     
+    /// <summary>
+    /// This method checks if a warehouse exists by its address, city, postal code, and profile ID.
+    /// </summary>
+    Task<bool> ExistsByAddressStreetAndAddressCityAndAddressPostalCodeIgnoreCaseAndProfileId(string address, string city, string postalCode, ProfileId profileId);
 }
