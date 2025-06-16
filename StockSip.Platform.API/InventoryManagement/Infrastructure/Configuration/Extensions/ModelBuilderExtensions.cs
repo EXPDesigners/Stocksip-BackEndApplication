8e8b67637f5b2446784e60f514c73f2b3bd0c1f2
Far14z <faridce14@gmail.com>
2025-06-16 18:43:39 -0500

    feat(warehouse): add orm mapping rules extension.

---
+++ StockSip.Platform.API/InventoryManagement/Infrastructure/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
+
+namespace StockSip.Platform.API.InventoryManagement.Infrastructure.Configuration.Extensions;
+
+/// <summary>
+/// This static class contains extension methods for the ModelBuilder to apply configuration for the Inventory Management domain model.
+/// </summary>
+public static class ModelBuilderExtensions
+{
+    public static void ApplyInventoryManagementConfiguration(this ModelBuilder builder)
+    {
+        // Inventory Management ORM Mapping Rules
+        builder.Entity<Warehouse>().HasKey(w => w.WarehouseId);
+        builder.Entity<Warehouse>().Property(w => w.WarehouseId).ValueGeneratedOnAdd();
+        builder.Entity<Warehouse>().Property(w => w.Name).IsRequired().HasMaxLength(100);
+        
+        builder.Entity<Warehouse>().OwnsOne( w => w.Address, a =>
+        {
+            a.WithOwner();
+            a.Property(ad => ad.Street).IsRequired().HasMaxLength(200);
+            a.Property(ad => ad.District).IsRequired().HasMaxLength(100);
+            a.Property(ad => ad.City).IsRequired().HasMaxLength(100);
+            a.Property(ad => ad.Country).IsRequired().HasMaxLength(100);
+            a.Property(ad => ad.PostalCode).IsRequired().HasMaxLength(10);
+        });
+        
+        builder.Entity<Warehouse>().OwnsOne(w => w.Temperature, t =>
+        {
+            t.WithOwner();
+            t.Property(temp => temp.MinTemperature).IsRequired();
+            t.Property(temp => temp.MaxTemperature).IsRequired();
+        });
+        
+        builder.Entity<Warehouse>().OwnsOne(w => w.Capacity, c =>
+        {
+            c.WithOwner();
+            c.Property(c => c.TotalCapacity).IsRequired();
+        });
+        
+        builder.Entity<Warehouse>().OwnsOne(w => w.ImageUrl, i =>
+        {
+            i.WithOwner();
+            i.Property(img => img.ImageUri).IsRequired().HasMaxLength(500);
+        });
+        
+        builder.Entity<Warehouse>().Property(w => w.ProfileId).IsRequired();
+        
+    }
+    
+}
--- StockSip.Platform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ StockSip.Platform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,5 +1,6 @@
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
+using StockSip.Platform.API.InventoryManagement.Infrastructure.Configuration.Extensions;
 using StockSip.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 namespace StockSip.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -19,6 +20,9 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        // Apply configuration for the Inventory Management bounded context
+        builder.ApplyInventoryManagementConfiguration();
         
         // Use snake case naming convention for the database
         builder.UseSnakeCaseNamingConvention();
