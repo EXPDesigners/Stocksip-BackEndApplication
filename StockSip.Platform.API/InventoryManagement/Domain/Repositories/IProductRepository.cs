fcd26f907062ed74bca063290f41330e173b85df
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-17 18:22:11 -0500

    feat(products): add outbound service contracts.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,32 @@
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.ValueObjects;
+using StockSip.Platform.API.Shared.Domain.Repositories;
+
+namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Repositories;
+
+/// <summary>
+/// This interface defines the contract for a repository that manages Product aggregates.
+/// </summary>
+public interface IProductRepository : IBaseRepository<Product>
+{
+    /// <summary>
+    /// This method retrieves all products associated with a specific provider ID.
+    /// </summary>
+    /// <returns>
+    /// An enumerable collection of Product aggregates that belong to the specified provider.
+    /// </returns>
+    Task<IEnumerable<Product>> FindByProviderIdAsync(ProviderId providerId);
+        
+    /// <summary>
+    /// This method retrieves all products associated with a specific warehouse ID.
+    /// </summary>
+    /// <returns>
+    /// An enumerable collection of Product aggregates that belong to the specified warehouse.
+    /// </returns>
+    Task<IEnumerable<Product>> FindByWarehouseIdAsync(string warehouseId);
+    
+    /// <summary>
+    /// This method checks if a product with the specified ID exists in the database.
+    /// </summary>
+    Task<bool> ExistsByIdAsync(string productId);
+}
