1e0d4d622d5ab3b1db41d2b7365d44ce7234e38e
Far14z <faridce14@gmail.com>
2025-06-16 20:49:24 -0500

    feat(warehouse): add query service contract.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/Services/IWarehouseQueryService.cs
@@ -0,0 +1,9 @@
+﻿using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Queries;
+
+namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Services;
+
+public interface IWarehouseQueryService
+{
+    Task<Warehouse?> Handle(GetWarehouseByIdQuery query);
+}
