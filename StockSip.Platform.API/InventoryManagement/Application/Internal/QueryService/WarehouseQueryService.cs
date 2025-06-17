a2ff9b65e5066858d8df79406a2d84936466ef59
Far14z <faridce14@gmail.com>
2025-06-16 20:49:49 -0500

    feat(warehouse): add query service service.

---
+++ StockSip.Platform.API/InventoryManagement/Application/Internal/QueryService/WarehouseQueryService.cs
@@ -0,0 +1,14 @@
+﻿using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Queries;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Repositories;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Services;
+
+namespace StockSip.Platform.API.InventoryManagement.Application.Internal.QueryService;
+
+public class WarehouseQueryService(IWarehouseRepository warehouseRepository) : IWarehouseQueryService
+{
+    public async Task<Warehouse?> Handle(GetWarehouseByIdQuery query)
+    {
+        return await warehouseRepository.FindByIdAsync(query.WarehouseId);
+    }
+}
