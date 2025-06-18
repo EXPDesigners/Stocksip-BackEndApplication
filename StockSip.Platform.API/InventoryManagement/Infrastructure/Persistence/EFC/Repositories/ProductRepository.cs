afe7994939baf37f093925729918e5d90b8fe7e7
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-18 11:56:30 -0500

    fix: add description for methods.

--- StockSip.Platform.API/InventoryManagement/Infrastructure/Persistence/EFC/Repositories/ProductRepository.cs
+++ StockSip.Platform.API/InventoryManagement/Infrastructure/Persistence/EFC/Repositories/ProductRepository.cs
@@ -8,8 +8,20 @@ using StockSip.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 
 namespace StockSip.Platform.API.InventoryManagement.Infrastructure.Persistence.EFC.Repositories;
 
+/// <summary>
+/// This class implements the IProductRepository interface, providing methods to interact with the Product aggregate.
+/// </summary>
 public class ProductRepository(AppDbContext context) : BaseRepository<Product>(context), IProductRepository
 {
+    /// <summary>
+    /// This async method retrieves all products associated with a specific provider ID.
+    /// </summary>
+    /// <param name="providerId">
+    /// The ID of the provider whose products are to be retrieved.
+    /// </param>
+    /// <returns>
+    /// A list of products that belong to the specified provider.
+    /// </returns>
     public async Task<IEnumerable<Product>> FindByProviderIdAsync(ProviderId providerId)
     {
         return await Context.Set<Product>()
@@ -17,6 +29,15 @@ public class ProductRepository(AppDbContext context) : BaseRepository<Product>(c
             .ToListAsync();
     }
 
+    /// <summary>
+    /// This async method retrieves all products associated with a specific warehouse ID.
+    /// </summary>
+    /// <param name="warehouseId">
+    /// The ID of the warehouse whose products are to be retrieved.
+    /// </param>
+    /// <returns>
+    /// A list of products that belong to the specified warehouse.
+    /// </returns>
     public async Task<IEnumerable<Inventory>> FindByWarehouseIdAsync(string warehouseId)
     {
         return await Context.Set<Inventory>()
@@ -25,6 +46,15 @@ public class ProductRepository(AppDbContext context) : BaseRepository<Product>(c
             .ToListAsync();
     }
 
+    /// <summary>
+    /// This async method checks if a product with the specified ID exists in the database.
+    /// </summary>
+    /// <param name="productId">
+    /// The ID of the product to check for existence.
+    /// </param>
+    /// <returns>
+    /// True if a product with the specified ID exists; otherwise, false.
+    /// </returns>
     public async Task<bool> ExistsByIdAsync(string productId)
     {
         return await Context.Set<Product>().AnyAsync(product => product.Id == productId);
