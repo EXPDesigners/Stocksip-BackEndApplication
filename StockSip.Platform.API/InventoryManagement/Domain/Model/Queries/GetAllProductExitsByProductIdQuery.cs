21cba19f1082592af8be6a79d6c812bd9da9f994
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-19 11:25:31 -0500

    feat(product-exits): add queries.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/Queries/GetAllProductExitsByProductIdQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Queries;
+
+/// <summary>
+/// This query is used to retrieve all product exits associated with a specific product.
+/// </summary>
+/// <param name="ProductId">
+/// The unique identifier of the product for which exits are being queried.
+/// </param>
+public record GetAllProductExitsByProductIdQuery(string ProductId);
---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/Queries/GetProductExitByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Queries;
+
+/// <summary>
+/// THis query is used to retrieve a product exit by its ID.
+/// </summary>
+/// <param name="ProductExitId">
+/// The unique identifier of the product exit.
+/// </param>
+public record GetProductExitByIdQuery(string ProductExitId);
