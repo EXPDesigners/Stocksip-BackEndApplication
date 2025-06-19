26500cee1d8351f2d52e1d03138cb895dd16afba
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-19 12:00:28 -0500

    feat(product-exits): add product exit resources.

---
+++ StockSip.Platform.API/InventoryManagement/Interfaces/REST/Resources/ProductExitResource.cs
@@ -0,0 +1,6 @@
+﻿namespace StockSip.Platform.API.InventoryManagement.Interfaces.REST.Resources;
+
+/// <summary>
+/// This record defines the product exit resource.
+/// </summary>
+public record ProductExitResource(string Id, string ProductId, string WarehouseId, string Quantity, DateTime ProductExpirationDate, DateTime ExitDate);
