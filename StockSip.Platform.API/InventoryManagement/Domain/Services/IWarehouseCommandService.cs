63a4c7719dadc4af31a03697cba4228513d3114b
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-19 11:31:02 -0500

    feat(product-exits): add command and query services

--- StockSip.Platform.API/InventoryManagement/Domain/Services/IWarehouseCommandService.cs
+++ StockSip.Platform.API/InventoryManagement/Domain/Services/IWarehouseCommandService.cs
@@ -1,5 +1,6 @@
 ﻿using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
 using StockSip.Platform.API.InventoryManagement.Domain.Model.Commands;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Entities;
 
 namespace StockSip.Platform.API.InventoryManagement.Domain.Services;
 
@@ -7,4 +8,5 @@ public interface IWarehouseCommandService
 {
     Task<Warehouse?> Handle(CreateWarehouseCommand command);
     Task<Warehouse?> Handle(UpdateWarehouseCommand command);
+    Task<ProductExit?> Handle(RegisterProductExitCommand command);
 }
--- StockSip.Platform.API/InventoryManagement/Domain/Services/IWarehouseQueryService.cs
+++ StockSip.Platform.API/InventoryManagement/Domain/Services/IWarehouseQueryService.cs
@@ -1,4 +1,5 @@
 ﻿using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Entities;
 using StockSip.Platform.API.InventoryManagement.Domain.Model.Queries;
 
 namespace StockSip.Platform.API.InventoryManagement.Domain.Services;
@@ -6,4 +7,6 @@ namespace StockSip.Platform.API.InventoryManagement.Domain.Services;
 public interface IWarehouseQueryService
 {
     Task<Warehouse?> Handle(GetWarehouseByIdQuery query);
+    Task<IEnumerable<ProductExit>> Handle(GetAllProductExitsByWarehouseIdQuery query);
+    Task<IEnumerable<ProductExit>> Handle(GetAllProductExitsByProductIdAndWarehouseIdQuery query);
 }
