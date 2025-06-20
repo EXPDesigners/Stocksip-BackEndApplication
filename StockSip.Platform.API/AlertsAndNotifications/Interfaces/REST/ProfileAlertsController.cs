eaf6a32a32767465c3a7188f4039c3c96abf0367
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-20 12:14:30 -0500

    feat(alerts-and-notifications): add inbound services.

---
+++ StockSip.Platform.API/AlertsAndNotifications/Interfaces/REST/ProfileAlertsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.Queries;
+using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.ValueObjects;
+using StockSip.Platform.API.AlertsAndNotifications.Domain.Services;
+using StockSip.Platform.API.AlertsAndNotifications.Interfaces.REST.Resources;
+using StockSip.Platform.API.AlertsAndNotifications.Interfaces.REST.Transform;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace StockSip.Platform.API.AlertsAndNotifications.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/profiles/{profileId}/alerts")]
+[Produces(MediaTypeNames.Application.Json)]
+[Tags("Profiles")]
+public class ProfileAlertsController(
+    IAlertQueryService alertQueryService
+    ) : ControllerBase
+{
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get all alerts by profile ID",
+        Description = "Retrieves all alerts associated with a specific profile ID.",
+        OperationId = "GetAlertsByProfileId")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Returns all alerts by profile ID.", typeof(AlertResource))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "No alerts found for the specified profile.")]
+    public async Task<IActionResult> GetAlertsByProfileId(string profileId)
+    {
+        var targetProfileId = new ProfileId(profileId);
+        var getAllAlertsByProfileIdQuery = new GetAllAlertsByProfileIdQuery(targetProfileId);
+        var alerts = await alertQueryService.Handle(getAllAlertsByProfileIdQuery);
+        var enumerable = alerts.ToList();
+        if (enumerable.Count == 0)
+        {
+            return NotFound(new { Message = "No alerts found for the specified profile." });
+        }
+        var alertResources = enumerable.Select(AlertResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(alertResources);
+    }
+}
