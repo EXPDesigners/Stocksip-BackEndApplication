3e60d48a61c318df50c201ea034934ef5e3d4a7f
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-19 20:22:11 -0500

    feat(alerts): add value objects.

--- StockSip.Platform.API/AlertsAndNotifications/Domain/Model/ValueObjects/ESeverityTypes.cs
+++ StockSip.Platform.API/AlertsAndNotifications/Domain/Model/ValueObjects/ESeverityTypes.cs
@@ -5,7 +5,7 @@
 /// The possible values are:
 /// - CRITICAL: Indicates a critical alert that requires immediate attention.
 /// - WARNING: Indicates a warning alert that should be addressed but is not immediately critical.
-/// - INFO: Indicates an informational alert that does not require immediate action but is useful to know.
+/// - INFO: Indicates an informational alert that does not require immediate action but is useful to know. If the alert is a type of this, then it transforms itself into a notification.
 /// </summary>
 public enum ESeverityTypes
 {
---
+++ StockSip.Platform.API/AlertsAndNotifications/Domain/Model/ValueObjects/ProfileId.cs
@@ -0,0 +1,26 @@
+﻿namespace StockSip.Platform.API.AlertsAndNotifications.Domain.Model.ValueObjects;
+
+/// <summary>
+/// This record defines the identifier of a profile that will receive alerts and notifications.
+/// </summary>
+public record ProfileId()
+{
+    /// <summary>
+    /// The unique identifier for the profile.
+    /// </summary>
+    public string Id { get; }
+
+    /// <summary>
+    /// The default constructor for the ProfileId record.
+    /// </summary>
+    /// <param name="id">The unique identifier for the profile</param>
+    /// <exception cref="ArgumentException"> Profile ID must be a non-empty string. </exception>
+    public ProfileId(string id) : this()
+    {
+        if (id == null || id.Trim().Length == 0)
+        {
+            throw new ArgumentException("Profile ID must be a non-empty string.");
+        }
+        Id = id;
+    }
+}
