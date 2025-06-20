09a95bc3843a00a06ace08955c95f1379a3ccf6c
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-20 12:47:42 -0500

    feat(alerts-and-notifications): add alert creation for product problem event.

---
+++ StockSip.Platform.API/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using Cortex.Mediator.Notifications;
+using StockSip.Platform.API.Shared.Domain.Model.Events;
+
+namespace StockSip.Platform.API.Shared.Application.Internal.EventHandlers;
+
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
+{
+}
