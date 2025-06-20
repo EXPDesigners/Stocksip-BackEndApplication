2caef51fa2a0ddd791858b6139587acdc39c74d1
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-20 12:23:25 -0500

    feat(alerts-and-notifications): add anti-corruption layer implementation.

--- StockSip.Platform.API/AlertsAndNotifications/Application/ACL/AlertsAndNotificationsContextFacade.cs
+++ StockSip.Platform.API/AlertsAndNotifications/Application/ACL/AlertsAndNotificationsContextFacade.cs
@@ -1,6 +1,44 @@
-﻿namespace StockSip.Platform.API.AlertsAndNotifications.Application.ACL;
+﻿using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.Commands;
+using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.ValueObjects;
+using StockSip.Platform.API.AlertsAndNotifications.Domain.Services;
+using StockSip.Platform.API.AlertsAndNotifications.Interfaces.ACL;
 
-public class AlertsAndNotificationsContextFacade
+namespace StockSip.Platform.API.AlertsAndNotifications.Application.ACL;
+
+/// <summary>
+/// This class serves as a facade for the Alerts and Notifications context, providing methods to create alerts for other contexts.
+/// </summary>
+/// <param name="alertCommandService">
+/// The command service for handling alert operations.
+/// </param>
+/// <param name="alertQueryService">
+/// The query service for retrieving alert information.
+/// </param>
+public class AlertsAndNotificationsContextFacade(
+    IAlertCommandService alertCommandService,
+    IAlertQueryService alertQueryService
+    ) : IAlertsAndNotificationsContextFacade
 {
-    
+    /// <summary>
+    /// This method creates an alert with the specified parameters.
+    /// It is used to receive information from other contexts and create an alert.
+    /// </summary>
+    /// <returns>
+    /// The ID of the created alert, or an empty string if the alert could not be created.
+    /// </returns>
+    public async Task<string> CreateAlert(string title, string message, string severity, string type, ProfileId profileId, ProductId productId,
+        WarehouseId warehouseId)
+    {
+        var createAlertCommand = new CreateAlertCommand(
+            title,
+            message,
+            severity,
+            type,
+            profileId,
+            productId,
+            warehouseId
+        );
+        var alert = await alertCommandService.Handle(createAlertCommand);
+        return alert?.Id ?? "";
+    }
 }
