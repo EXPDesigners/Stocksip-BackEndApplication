e0e8fe1e9286a1a128917adb1d7caf837b64f432
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-19 20:59:19 -0500

    feat(alerts): add command and query service contracts.

---
+++ StockSip.Platform.API/AlertsAndNotifications/Domain/Services/IAlertQueryService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.Aggregates;
+using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.Queries;
+
+namespace StockSip.Platform.API.AlertsAndNotifications.Domain.Services;
+
+public interface IAlertQueryService
+{
+    Task<Alert?> Handle(GetAlertByIdQuery query);
+    
+    Task<IEnumerable<Alert>> Handle(GetAllAlertsByProductIdQuery query);
+    Task<IEnumerable<Alert>> Handle(GetAllAlertsByProfileIdQuery query);
+    Task<IEnumerable<Alert>> Handle(GetAllAlertsByWarehouseIdQuery query);
+}
