e3e70f4f6ebba78ac1d73e8ca49c4fb586b7ca7b
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-19 20:55:45 -0500

    feat(alerts-and-notifications): add outbound service contract for alerts.

---
+++ StockSip.Platform.API/AlertsAndNotifications/Domain/Repositories/IAlertRepository.cs
@@ -0,0 +1,26 @@
+﻿using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.Aggregates;
+using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.ValueObjects;
+using StockSip.Platform.API.Shared.Domain.Repositories;
+
+namespace StockSip.Platform.API.AlertsAndNotifications.Domain.Repositories;
+
+/// <summary>
+/// This interface defines the contract for a repository that manages Alert aggregates.
+/// </summary>
+public interface IAlertRepository : IBaseRepository<Alert>
+{
+    /// <summary>
+    /// This method retrieves all alerts associated with a specific product ID.
+    /// </summary>
+    Task<IEnumerable<Alert>> FindByProductIdAsync(ProductId productId);
+    
+    /// <summary>
+    /// This method retrieves all alerts associated with a specific profile ID.
+    /// </summary>
+    Task<IEnumerable<Alert>> FindByProfileIdAsync(ProfileId profileId);
+    
+    /// <summary>
+    /// This method retrieves all alerts associated with a specific warehouse ID.
+    /// </summary>
+    Task<IEnumerable<Alert>> FindByWarehouseIdAsync(WarehouseId warehouseId);
+}
