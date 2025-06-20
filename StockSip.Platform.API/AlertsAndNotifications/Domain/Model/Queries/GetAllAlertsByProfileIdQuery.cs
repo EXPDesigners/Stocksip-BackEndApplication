07be45a159666e33175aea5be7b5b06597a009ae
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-19 20:48:26 -0500

    feat(alerts): add alert query.

---
+++ StockSip.Platform.API/AlertsAndNotifications/Domain/Model/Queries/GetAllAlertsByProfileIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.ValueObjects;
+
+namespace StockSip.Platform.API.AlertsAndNotifications.Domain.Model.Queries;
+
+/// <summary>
+/// This query is used to retrieve all alerts associated with a specific profile.
+/// </summary>
+/// <param name="ProfileId">
+/// The unique identifier of the profile for which alerts are being requested.
+/// </param>
+public record GetAllAlertsByProfileIdQuery(ProfileId ProfileId);
