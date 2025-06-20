79f5cf96a5e680d04f0875e89186c0f9ea88f153
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-20 11:06:12 -0500

    feat(alerts): add alert assemblers.

--- StockSip.Platform.API/AlertsAndNotifications/Interfaces/REST/Resources/MarkAlertAsReadResource.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace StockSip.Platform.API.AlertsAndNotifications.Interfaces.REST.Resources;
-
-/// <summary>
-/// This record defines the resource for marking an alert as read.
-/// </summary>
-public record MarkAlertAsReadResource(string AlertId);
