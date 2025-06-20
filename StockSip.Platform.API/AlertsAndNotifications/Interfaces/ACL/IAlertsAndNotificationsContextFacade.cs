e9d62c7aa4c47e7bd382f36355df4801ba3b37df
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-20 12:19:55 -0500

    feat(alerts-and-notifications): add anti-corruption layer.

---
+++ StockSip.Platform.API/AlertsAndNotifications/Interfaces/ACL/IAlertsAndNotificationsContextFacade.cs
@@ -0,0 +1,21 @@
+﻿using StockSip.Platform.API.AlertsAndNotifications.Domain.Model.ValueObjects;
+
+namespace StockSip.Platform.API.AlertsAndNotifications.Interfaces.ACL;
+
+/// <summary>
+/// This interface defines the contract for the Alerts and Notifications Context Facade.
+/// </summary>
+public interface IAlertsAndNotificationsContextFacade
+{
+    /// <summary>
+    /// This method creates a new alert in the system with the specified parameters.
+    /// </summary>
+    Task<string> CreateAlert(
+        string title,
+        string message,
+        string severity,
+        string type,
+        ProfileId profileId,
+        ProductId productId,
+        WarehouseId warehouseId);
+}
