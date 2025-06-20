2eb9e8133f1ef7de3a8e3d9f202779eb495dd966
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-19 20:50:07 -0500

    feat(alerts): add query to retrieve an alert by its id.

---
+++ StockSip.Platform.API/AlertsAndNotifications/Domain/Model/Queries/GetAlertByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace StockSip.Platform.API.AlertsAndNotifications.Domain.Model.Queries;
+
+/// <summary>
+/// This query is used to retrieve an alert by its unique identifier.
+/// </summary>
+/// <param name="AlertId">
+/// The unique identifier of the alert to be retrieved.
+/// </param>
+public record GetAlertByIdQuery(string AlertId);
