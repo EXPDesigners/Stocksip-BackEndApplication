53d2a64c5a3eb5253c8fd6758342aca89d706063
Far14z <faridce14@gmail.com>
2025-06-21 23:41:39 -0500

    feat(subscription): add plan limits and plan type value object.

---
+++ StockSip.Platform.API/payment-and-subscription/Domain/ValueObjects/PlanType.cs
@@ -0,0 +1,9 @@
+﻿namespace StockSip.Platform.API.payment_and_subscription.domain.ValueObjects;
+
+public record PlanType(string Type)
+{
+    public static PlanType Free => new("Free");
+    public static PlanType Premium => new("Premium");
+    
+    public bool IsPremium => this == Premium;
+}
