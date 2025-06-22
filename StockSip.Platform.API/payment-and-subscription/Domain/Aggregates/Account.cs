6285c268de4e2ec26276cdcb94077036fec56d09
Far14z <faridce14@gmail.com>
2025-06-21 23:33:28 -0500

    chore: add code documentation.

--- StockSip.Platform.API/payment-and-subscription/Domain/Aggregates/Account.cs
+++ StockSip.Platform.API/payment-and-subscription/Domain/Aggregates/Account.cs
@@ -6,21 +6,33 @@ namespace StockSip.Platform.API.payment_and_subscription.domain.aggregates;
 /// <summary>
 /// This class encapsulates the properties and behaviors of an account, including its unique identifier and associated email.
 /// </summary>
-public class Account
+public class Account 
 {
-    /// <summary>
-    /// The account id represents the unique identifier of the account
-    /// </summary>
     public string AccountId { get; private set; } = Guid.NewGuid().ToString();
     
+    public AccountStatus AccountStatus { get; internal set; } = AccountStatus.Active;
+    
+    public Role Role { get; internal set; }
+    
+    public DateTime CreatedDate { get; internal set; } = DateTime.UtcNow;
+    
+    public UserId UserId { get; internal set; }
+    
+    public ProfileId ProfileId { get; internal set; }
+    
     /// <summary>
-    /// This email represents the general email that associated all the accounts 
+    /// Default constructor for EF Core.
     /// </summary>
-    public GeneralEmail Email { get; private set; }
+    private Account() {}
+
+
+    public Account(UserId userId, ProfileId profileId, Role role)
+    {
+        UserId = userId;
+        ProfileId = profileId;
+        Role = role;
+    }
     
-    public Role Role { get; private set; }
-    
-    public Plan PlanType { get; private set; }
     
     
 }
