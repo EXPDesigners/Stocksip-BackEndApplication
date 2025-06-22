27cbd1a097a98377eddbdf73aabd4be5250dd5e9
Far14z <faridce14@gmail.com>
2025-06-21 23:43:50 -0500

    feat(account): add usage counters value object.

---
+++ StockSip.Platform.API/payment-and-subscription/Domain/ValueObjects/UsageCounters.cs
@@ -0,0 +1,8 @@
+﻿namespace StockSip.Platform.API.payment_and_subscription.domain.ValueObjects;
+
+public record UsageCounters()
+{
+    public int CountWarehouses { get; init; }
+    public int CountProducts { get; init; }
+    
+}
