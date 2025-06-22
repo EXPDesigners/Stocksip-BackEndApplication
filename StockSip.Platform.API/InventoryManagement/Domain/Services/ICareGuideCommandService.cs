344f7814372c12ab7db8a5d1301e5cf967234589
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-21 21:52:46 -0500

    feat(care-guides): add care guide command service contract.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Services/ICareGuideCommandService.cs
@@ -0,0 +1,11 @@
+﻿using StockSip.Platform.API.InventoryManagement.Domain.Model.Commands;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Entities;
+
+namespace StockSip.Platform.API.InventoryManagement.Domain.Services;
+
+public interface ICareGuideCommandService
+{
+    Task<CareGuide?> Handle(CreateCareGuideCommand command);
+    Task<CareGuide?> Handle(CreateCareGuideWithoutProductIdCommand command);
+    Task<CareGuide?> Handle(UpdateCareGuideCommand command);
+}
