e21b334c2dca6607fc7c01fc527277f501f2c1b8
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-21 21:54:08 -0500

    feat(care-guides): add care guide query service contract.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Services/ICareGuideQueryService.cs
@@ -0,0 +1,12 @@
+﻿using StockSip.Platform.API.InventoryManagement.Domain.Model.Entities;
+using StockSip.Platform.API.InventoryManagement.Domain.Model.Queries;
+
+namespace StockSip.Platform.API.InventoryManagement.Domain.Services;
+
+public interface ICareGuideQueryService
+{
+    Task<IEnumerable<CareGuide>> Handle(GetAllCareGuidesByAccountId query);
+    
+    Task<CareGuide?> Handle(GetCareGuideByIdQuery query);
+    Task<CareGuide?> Handle(GetCareGuideByProductIdQuery query);
+}
