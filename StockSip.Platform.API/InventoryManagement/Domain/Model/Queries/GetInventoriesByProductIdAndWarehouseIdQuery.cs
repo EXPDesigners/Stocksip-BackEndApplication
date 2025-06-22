b7a66ad56c5329523ada27c53aa68dba2130c0fc
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-22 01:51:32 -0500

    feat(inventories): add queries.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Model/Queries/GetInventoriesByProductIdAndWarehouseIdQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace StockSip.Platform.API.InventoryManagement.Domain.Model.Queries;
+
+/// <summary>
+/// This command is used to retrieve inventories by its product and warehouse id.
+/// </summary>
+public record GetInventoriesByProductIdAndWarehouseIdQuery(string ProductId, string WarehouseId);
