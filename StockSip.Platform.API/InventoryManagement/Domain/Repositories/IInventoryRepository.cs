91084c6a0cfe92daa9f98da079928e793d825c28
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-22 02:06:28 -0500

    feat(inventories): add inventory repository contract.

---
+++ StockSip.Platform.API/InventoryManagement/Domain/Repositories/IInventoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using StockSip.Platform.API.InventoryManagement.Domain.Model.Aggregates;
+using StockSip.Platform.API.Shared.Domain.Repositories;
+
+namespace StockSip.Platform.API.InventoryManagement.Domain.Repositories;
+
+/// <summary>
+/// This interface defines the contract for a repository that manages Inventory aggregates.
+/// </summary>
+public interface IInventoryRepository : IBaseRepository<Inventory>
+{
+    /// <summary>
+    /// This method retrieves an inventory item by the product and warehouse ID.
+    /// </summary>
+    Task<Inventory> FindByProductIdAndWarehouseIdAsync(string productId, string warehouseId);
+    
+    /// <summary>
+    /// This method retrieves an inventory item by the product ID, warehouse ID and expiration date.
+    /// </summary>
+    Task<Inventory?> FindByProductIdAndWarehouseIdAndExpirationDateAsync(string productId, string warehouseId, DateTime expirationDate);
+
+    /// <summary>
+    /// This method checks if an inventory exists by the given product and warehouse ID.
+    /// </summary>
+    bool ExistsByProductIdAndWarehouseIdAsync(string productId, string warehouseId);
+}
