cc30aa5b3ff00fd24cabdc70512c237a4f104e21
Far14z <faridce14@gmail.com>
2025-06-21 23:42:03 -0500

    feat(subscriptions): add subscription aggregate.

---
+++ StockSip.Platform.API/payment-and-subscription/Domain/Aggregates/Subscription.cs
@@ -0,0 +1,27 @@
+﻿using StockSip.Platform.API.payment_and_subscription.domain.ValueObjects;
+
+namespace StockSip.Platform.API.payment_and_subscription.domain.aggregates;
+
+/// This aggregate represents a subscription in the payment and subscription domain.
+/// <summary>
+/// This class encapsulates the properties and behaviors of a subscription, including its unique identifier, plan ID, account ID, start date, end date, and status.
+/// </summary>
+public class Subscription
+{
+    public string SubscriptionId { get; private set; } = Guid.NewGuid().ToString();
+    
+    public string PlanId { get; set; }
+    
+    public string AccountId { get; set; }
+    
+    public DateTime StartDate { get; internal set; }
+    
+    public DateTime? EndDate { get; internal set; }
+
+    public EStatus Status { get; set; } = EStatus.ACTIVE;
+    
+    /// <summary>
+    /// Default constructor for EF Core.
+    /// </summary>
+    private Subscription() {}
+}
