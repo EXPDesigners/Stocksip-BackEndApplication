1380c1c1cdb63d3ea21a1fe583ede25718a8ed34
Far14z <faridce14@gmail.com>
2025-06-24 01:49:08 -0500

    feat(account): add subscription and plan entities.

---
+++ StockSip.Platform.API/PaymentAndSubscription/Domain/Model/Entities/Subscription.cs
@@ -0,0 +1,35 @@
+﻿using StockSip.Platform.API.payment_and_subscription.Domain.Model.Entities;
+using StockSip.Platform.API.PaymentAndSubscription.Domain.Model.Aggregates;
+
+namespace StockSip.Platform.API.PaymentAndSubscription.Domain.Model.Entities;
+
+/// This entity represents a subscription in the payment and subscription domain.
+/// <summary>
+/// This class encapsulates the properties and behaviors of a subscription, including its unique identifier, associated account, subscription plan, creation date, expiration date, and active status.
+/// </summary>
+public class Subscription
+{
+    public string SubscriptionId { get; private set; } = Guid.NewGuid().ToString();
+    
+    public Account Account { get; internal set; }
+    public string AccountId { get; private set; }
+    public SubscriptionPlan SubscriptionPlan { get; internal set; }
+    
+    public string SubscriptionPlanId { get; private set; }
+    
+    public DateTime CreatedDate { get; internal set; }
+    public DateTime ExpiredDate { get; internal set; }
+    public bool IsActive => ExpiredDate > DateTime.UtcNow;
+    
+    /// <summary>
+    /// Default constructor for EF Core.
+    /// </summary>
+    public Subscription() {}
+    
+    public Subscription(string accountId, SubscriptionPlan subscriptionPlan, DateTime expiredDate)
+    {
+        AccountId = accountId;
+        SubscriptionPlan = subscriptionPlan;
+        ExpiredDate = expiredDate;
+    }
+}
---
+++ StockSip.Platform.API/PaymentAndSubscription/Domain/Model/Entities/SubscriptionPlan.cs
@@ -0,0 +1,38 @@
+﻿using StockSip.Platform.API.PaymentAndSubscription.Domain.Model.ValueObjects;
+using StockSip.Platform.API.Shared.Domain.Model.ValueObjects;
+
+namespace StockSip.Platform.API.payment_and_subscription.Domain.Model.Entities;
+
+/// This entity represents a subscription plan in the payment and subscription domain.
+/// <summary>
+/// This class encapsulates the properties and behaviors of a subscription plan, including its unique identifier, plan type, duration, maximum warehouses, maximum products, and price.
+/// </summary>
+public class SubscriptionPlan
+{
+    public string SubscriptionId { get; private set; }
+    
+    public PlanType PlanType { get; private set; }
+    
+    public TimeSpan Duration { get; private set; }
+    
+    public int MaxWarehouses { get; private set; }
+    
+    public int MaxProducts { get; private set; }
+    
+    public Money Price { get; internal set; }
+    
+    /// <summary>
+    /// Default constructor for EF Core.
+    /// </summary>
+    public SubscriptionPlan() {}
+
+    public SubscriptionPlan (string name, TimeSpan duration, int maxWarehouses, int maxProducts, Money price)
+    {
+        SubscriptionId = Guid.NewGuid().ToString();
+        PlanType = new PlanType(name);
+        Duration = duration;
+        MaxWarehouses = maxWarehouses;
+        MaxProducts = maxProducts;
+        Price = price;
+    }
+}
