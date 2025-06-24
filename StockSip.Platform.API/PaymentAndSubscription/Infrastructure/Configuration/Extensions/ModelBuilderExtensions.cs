055dcaa4c50e4e8d2497e6f95babff25694c5b02
Far14z <faridce14@gmail.com>
2025-06-24 01:58:50 -0500

    feat(account): add account repository contract and implementation.

---
+++ StockSip.Platform.API/PaymentAndSubscription/Infrastructure/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StockSip.Platform.API.PaymentAndSubscription.Infrastructure.Configuration.Extensions;
+
+/// <summary>
+/// This class contains extension methods for the ModelBuilder to apply configurations related to Payment and Subscription domain models.
+/// </summary>
+public static class ModelBuilderExtensions
+{
+    public static void ApplyPaymentAndSubscriptionConfiguration(this ModelBuilder builder)
+    {
+        // Payment and Subscription Domain Configuration
+        
+        // Account ORM Mapping Rules
+        
+        // Subscription ORM Mapping Rules
+        
+        // Subscription Plan ORM Mapping Rules
+    }
+}
---
+++ StockSip.Platform.API/PaymentAndSubscription/Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using StockSip.Platform.API.PaymentAndSubscription.Domain.Model.Aggregates;
+using StockSip.Platform.API.PaymentAndSubscription.Domain.Repositories;
+using StockSip.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using StockSip.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace StockSip.Platform.API.PaymentAndSubscription.Infrastructure.Repositories;
+
+public class AccountRepository(AppDbContext context) : BaseRepository<Account>(context), IAccountRepository
+{
+    
+}
