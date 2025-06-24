adafe7eee6efacf6e53600a9dbd10baa626ffb69
Far14z <faridce14@gmail.com>
2025-06-24 17:11:35 -0500

    feat(account): add command for account, subscription and plan.

--- StockSip.Platform.API/PaymentAndSubscription/Domain/Model/Commands/CreateAccountCommand.cs
+++ StockSip.Platform.API/PaymentAndSubscription/Domain/Model/Commands/CreateAccountCommand.cs
@@ -1,3 +1,9 @@
 ﻿namespace StockSip.Platform.API.PaymentAndSubscription.Domain.Model.Commands;
 
-public record CreateAccountCommand(string UserId, string ProfileId, string Role);
+/// <summary>
+/// This command is used to create a new account in the payment and subscription domain.
+/// </summary>
+/// <param name="username">The username for the new user.</param>
+/// <param name="password">The password for the new user.</param>
+/// <param name="role">The role assigned to the new account.</param>
+public record CreateAccountCommand(string username, string password, string role);
---
+++ StockSip.Platform.API/PaymentAndSubscription/Domain/Model/Commands/SeedPlansCommand.cs
@@ -0,0 +1,6 @@
+namespace StockSip.Platform.API.PaymentAndSubscription.Domain.Model.Commands;
+
+/// <summary>
+/// This command is used to seed data for plans in the system.
+/// </summary>
+public record SeedPlansCommand();
