19153cd46e590a48c433ce2bba03005f55f87108
Far14z <faridce14@gmail.com>
2025-06-26 00:44:32 -0500

    feat(cloudinary): add cloudinary configuration.

---
+++ StockSip.Platform.API/InventoryManagement/Infrastructure/FileStorage/Cloudinary/Configuration/CloudinarySettings.cs
@@ -0,0 +1,10 @@
+﻿namespace StockSip.Platform.API.InventoryManagement.Infrastructure.FileStorage.Cloudinary.Configuration;
+
+public class CloudinarySettings
+{
+    public string CloudName { get; set; } = null;
+    
+    public string ApiKey { get; set; } = null;
+    
+    public string ApiSecret { get; set; } = null;
+}
